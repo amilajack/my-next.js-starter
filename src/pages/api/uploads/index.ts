8b11e70d79cb08484f1b6becb0f29be5855311ed
Amila Welihinda <amilajack@gmail.com>
2022-07-27 17:42:14 -0700

    misc

--- src/pages/api/uploads/index.ts
+++ src/pages/api/uploads/index.ts
@@ -9,15 +9,22 @@ export const config = {
 };
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
   const form = new formidable.IncomingForm();
 
-  await new Promise((resolve, reject) => {
-    form.parse(req, (err, fields, files) => {
-      console.log(err, fields, files);
-      if (err) reject(err);
-      else resolve(files);
-    });
+  // await new Promise((resolve, reject) => {
+  //   form.parse(req, (err, fields, files) => {
+  //     console.log(err, fields, files);
+  //     if (err) reject(err);
+  //     else resolve(files);
+  //   });
+  // });
+
+  form.parse(req, async function (err, fields, files) {
+    // await saveFile(files.file);
+    console.log(files);
+    return res.status(201).send('');
   });
 
-  res.json({ message: 'Hello World' });
+  // return res.json({ message: 'Hello World' });
 };
