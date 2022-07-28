e90b93849ea3a80753866b7005cc96f87aaec324
Amila Welihinda <amilajack@gmail.com>
2022-07-27 18:54:36 -0700

    rawbody

--- src/pages/api/uploads/index.ts
+++ src/pages/api/uploads/index.ts
@@ -1,5 +1,5 @@
 // @ts-nocheck
-import formidable from 'formidable-serverless';
+// import formidable from 'formidable-serverless';
 import { NextApiRequest, NextApiResponse } from 'next';
 
 export const config = {
@@ -10,16 +10,26 @@ export const config = {
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
-  const form = new formidable.IncomingForm();
-  const filesEntries = [];
 
-  form.onPart = (part) => {
-    part.on('data', (buffer) => {
-      filesEntries.push([part.name!, buffer]);
-    });
-  };
-  form.parse(req, async function (err, fields, files) {
-    console.log(files, fields, filesEntries);
-    return res.status(201).send('');
+  req.rawBody = '';
+  req.setEncoding('utf8');
+
+  req.on('data', function (chunk) {
+    req.rawBody += chunk;
   });
+
+  req.on('end', function () {
+    console.log(req.rawBody.length);
+    return res.status(201).send(req.rawBody.length);
+  });
+
+  // form.onPart = (part) => {
+  //   part.on('data', (buffer) => {
+  //     filesEntries.push([part.name!, buffer]);
+  //   });
+  // };
+  // form.parse(req, async function (err, fields, files) {
+  //   console.log(files, fields, filesEntries);
+  //   return res.status(201).send('');
+  // });
 };
